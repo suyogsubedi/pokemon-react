217662e28e2a07e1b5e87c61d810297841773140
Suyog Subedi <subedisuyog555@gmail.com>
2020-09-04 08:59:27 +0545

    Updates

---
+++ precache-manifest.8944eb95874cf601e19b5aa1c20c2fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fa981c76f731205f1c1a0a3a1fef4b",
+    "url": "/pokemon-react/index.html"
+  },
+  {
+    "revision": "60eb7238b1593e2466b5",
+    "url": "/pokemon-react/static/css/main.4fa97a6f.chunk.css"
+  },
+  {
+    "revision": "0430d70d65607c9fbcce",
+    "url": "/pokemon-react/static/js/2.e3cf8b24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-react/static/js/2.e3cf8b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60eb7238b1593e2466b5",
+    "url": "/pokemon-react/static/js/main.454589bd.chunk.js"
+  },
+  {
+    "revision": "74b9e2ab7c0a2bb859a7",
+    "url": "/pokemon-react/static/js/runtime-main.cc2ecccc.js"
+  }
+]);
